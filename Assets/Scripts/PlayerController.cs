9cee2843c78a2a69c56b84e26548a106b71c9fef
Tianhao Zhang <zhangth1990@hotmail.com>
2022-04-07 23:53:46 -0700

    add position rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,20 +14,55 @@ public class PlayerController : MonoBehaviour
     float yRange = 1f;
 
     // Update is called once per frame
+    [SerializeField]
+    float positionPitchFactor = -2f;
+
+    [SerializeField]
+    float controlPitchFactor = -10f;
+
+    [SerializeField]
+    float positionYawFactor = 2f;
+
+    [SerializeField]
+    float controlRollFactor = -20f;
+
+    float xThrow;
+
+    float yThrow;
+
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal");
-        float verticalThrow = Input.GetAxis("Vertical");
+        ProcessTranslation();
+        ProcessRotation();
+    }
 
-        float xOffset = horizontalThrow * Time.deltaTime * controlSpeed;
+    void ProcessTranslation()
+    {
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
+
+        float xOffset = xThrow * Time.deltaTime * controlSpeed;
         float rawXPos = transform.localPosition.x + xOffset;
         float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
 
-        float yOffset = verticalThrow * Time.deltaTime * controlSpeed;
+        float yOffset = yThrow * Time.deltaTime * controlSpeed;
         float rawYPos = transform.localPosition.y + yOffset;
         float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
 
         transform.localPosition =
             new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+
+    void ProcessRotation()
+    {
+        float pitchDueToPosition =
+            transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
